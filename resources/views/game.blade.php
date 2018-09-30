c504404fc1d4fb176598cfefd05d5de414d9eebf
shalvah <diakon.ng@gmail.com>
2018-10-01 00:10:06 +0100

    Update view

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -5,11 +5,14 @@
         <h2>@{{ game.score }}</h2>
         @auth
             <form>
+                <label for="minute">In what minute did this happen?</label>
+                <input class="form-control" type="number" id="minute">
+
                 <label for="type">Event type</label>
                 <input class="form-control" id="type" placeholder="Goal, foul, injury, booking...">
 
-                <label for="comment">Comment</label>
-                <input class="form-control" id="comment" placeholder="Add a description or comment...">
+                <label for="description">Description</label>
+                <input class="form-control" id="description" placeholder="Add a description or comment...">
 
                 <button type="submit" class="btn btn-primary">Post update</button>
             </form>
