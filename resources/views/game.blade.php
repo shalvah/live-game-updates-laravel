f70fce087401161b7685e8a4431ec3d178205d23
shalvah <diakon.ng@gmail.com>
2018-10-01 10:24:10 +0100

    Update single game view

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -1,22 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h2>@{{ game.score }}</h2>
+    <div id="main" class="container" xmlns:v-on="http://www.w3.org/1999/xhtml">
+        <h2>@{{ game.first_team }}
+            <span @auth contenteditable @endauth v-on:blur="updateFirstTeamScore">@{{ game.first_team_score }}</span>
+            -
+            <span @auth contenteditable @endauth v-on:blur="updateSecondTeamScore">@{{ game.second_team_score }}</span>
+            @{{ game.second_team }}</h2>
         @auth
-            <form>
-                <label for="minute">In what minute did this happen?</label>
-                <input class="form-control" type="number" id="minute">
+            <div class="card">
+                <div class="card-body">
+            <form v-on:submit="updateGame">
+                <h6>Post a new game update</h6>
+                <input class="form-control" type="number" id="minute" v-model="pendingUpdate.minute" placeholder="In what minute did this happen?">
 
-                <label for="type">Event type</label>
-                <input class="form-control" id="type" placeholder="Goal, foul, injury, booking...">
+                <input class="form-control" id="type" placeholder="Event type (goal, foul, injury, booking...)" v-model="pendingUpdate.type">
 
-                <label for="description">Description</label>
-                <input class="form-control" id="description" placeholder="Add a description or comment...">
+                <input class="form-control" id="description" placeholder="Add a description or comment..." v-model="pendingUpdate.description">
 
                 <button type="submit" class="btn btn-primary">Post update</button>
             </form>
+                </div>
+            </div>
         @endauth
+        <br>
+        <h4>Game updates</h4>
             <div class="card-body" v-for="update in updates">
                     <div class="card-title">
                         <h5>@{{ update.type }}</h5>
@@ -26,4 +34,8 @@
                     </div>
                 </div>
     </div>
+    <script>
+        window.updates = @json($updates);
+        window.game = @json($game);
+    </script>
 @endsection
