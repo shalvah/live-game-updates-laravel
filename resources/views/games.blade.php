9e7ac882112adcd4be26b032c7c027539b758db3
shalvah <diakon.ng@gmail.com>
2018-10-01 08:25:41 +0100

    Update form to create game

--- resources/views/games.blade.php
+++ resources/views/games.blade.php
@@ -4,8 +4,16 @@
     <div class="container">
         <h2>Ongoing games</h2>
         @auth
-        <button type="button" class="btn btn-primary">Start new game</button>
+            <form method="post" action="{{ url('/games') }}" class="form-inline">
+                @csrf
+                <input class="form-control" name="first_team" placeholder="First team" required>
+                <input class="form-control" name="second_team" placeholder="Second team" required>
+                <input type="hidden" name="first_team_score" value="0">
+                <input type="hidden" name="second_team_score" value="0">
+                <button type="submit" class="btn btn-primary">Start new game</button>
+            </form>
         @endauth
+        <br>
         @forelse($games as $game)
             <a class="card bg-dark" href="/games/{{ $game->id }}">
                 <div class="card-body">
