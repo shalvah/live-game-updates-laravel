b009365e9c99527b0a694e9e9392c3bb18fda9f1
shalvah <diakon.ng@gmail.com>
2018-10-01 13:21:55 +0100

    Publish updates to Pusher

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Game;
 use App\Update;
+use Pusher\Laravel\PusherManager;
 
 class HomeController extends Controller
 {
@@ -26,18 +27,21 @@ class HomeController extends Controller
         return redirect("/games/$game->id");
     }
 
-    public function updateGame(int $id)
+    public function updateGame(int $id, PusherManager $pusher)
     {
         $data = request()->all();
         $data['game_id'] = $id;
         $update = Update::create($data);
+        $pusher->trigger("game-updates-$id", 'event', $update, request()->header('x-socket-id'));
         return response()->json($update);
     }
 
-    public function updateScore(int $id)
+    public function updateScore(int $id, PusherManager $pusher)
     {
         $data = request()->all();
-        Game::where('id', $id)->update($data);
+        $game = Game::find($id);
+        $game->update($data);
+        $pusher->trigger("game-updates-$id", 'score', $game, request()->header('x-socket-id'));
         return response()->json();
     }
 }
