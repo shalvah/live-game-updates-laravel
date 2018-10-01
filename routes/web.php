218529cbbb40c77e2739fb89372338ca48b9854a
shalvah <diakon.ng@gmail.com>
2018-10-01 10:21:35 +0100

    Add route to update game score

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,4 @@ Route::get('/', 'HomeController@index')->name('home');
 Route::get('/games/{id}', 'HomeController@viewGame');
 Route::post('/games', 'HomeController@startGame')->middleware('auth');
 Route::post('/games/{id}', 'HomeController@updateGame')->middleware('auth');
+Route::post('/games/{id}/score', 'HomeController@updateScore')->middleware('auth');
