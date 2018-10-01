58c83b723aebc0223274c752e382914cecb2f485
shalvah <diakon.ng@gmail.com>
2018-10-01 08:00:24 +0100

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,5 @@ Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/games/{id}', 'HomeController@viewGame');
+Route::post('/games', 'HomeController@startGame')->middleware('auth');
 Route::post('/games/{id}', 'HomeController@updateGame')->middleware('auth');
